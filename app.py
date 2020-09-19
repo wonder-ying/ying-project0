dea4007eb1916e7d23c3e6e2299e91f305a12f7a
Ying Wan <beibeiwy8712@gmail.com>
2020-09-18 20:17:39 -0700

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import pandas as pd
-from threading import Thread
-from flask import Flask,render_template,request,redirect
-import simplejson as json
+from flask import Flask, render_template, request, redirect, abort, Response
 import requests
-import datetime
+import simplejson as json
+#import datetime
 import jinja2
+import pandas as pd
 
 from bokeh.embed import server_document, components 
 from bokeh.layouts import column
@@ -12,79 +11,73 @@ from bokeh.models import ColumnDataSource, HoverTool, TextInput, CustomJS
 from bokeh.io import curdoc
 from bokeh.plotting import figure, output_file, show
 from bokeh.server.server import Server
-from bokeh.themes import Theme
 
 app = Flask(__name__)
 
 app.vars={}
 
 @app.route('/')
-def main():
-   return redirect('/index')
-
-@app.route('/index', methods=['GET'])
+@app.route('/index')
 def index():
    return render_template('index.html')
-    
+
 @app.route('/graph', methods=['POST'])
 def graph():
-   # if request.method == 'POST':
    app.vars['symbol'] = request.form['symbol']
 
-   API_URL = "https://www.alphavantage.co/query" 
+   API_URL = "https://www.alphavantage.co/query"
    data = { "function": "TIME_SERIES_DAILY_ADJUSTED",
-            "symbol": app.vars['symbol'],
-            "outputsize" : "full",
-            "datatype": "json",
-            "apikey": "GH5U75JGWCE1C0C3" }
-   response = requests.get(API_URL, data)
-   response_json = response.json()
+             "symbol": app.vars['symbol'],
+             "apikey": "UBSVT7NO385KCUNM",
+             "outputsize": "full",
+             "datatype": "json"}
+   r = requests.get(API_URL, data)
+   r_json = r.json()
 
-   df = pd.DataFrame.from_dict(response_json['Time Series (Daily)'], orient= 'index').sort_index(axis=1)
-   df = df.rename(columns={ '1. open': 'Open', '2. high': 'High', '3. low': 'Low', '4. close': 'Close', '5. adjusted close': 'Adj Close', '6. volume': 'Volume', '7. dividend amount': 'Dividend Amount', '8. split coefficient': 'Split Coefficient'})
-   df.reset_index(inplace=True)
-   df['index'] = pd.to_datetime(df['index'])
-   mask = (df['index'] >= '2020-01-01')
-   df = df.loc[mask]
-   df1 = df[[ 'index', 'Open', 'Close', 'Adj Close']]
+   # Import data as Pandas df
+   df = pd.DataFrame.from_dict(r_json['Time Series (Daily)'], orient='index').sort_index(axis=1)
+   df.reset_index(inplace = True)
+   df = df.rename(columns={ 'index': 'Date','1. open': 'Open', '2. high': 'High', '3. low': 'Low', '4. close': 'Close', '5. adjusted close': 'Adjusted Close', '6. volume': 'Volume', '7. dividend amount': 'Dividend Amount', '8. split coefficient': 'Split Coefficient'})
+   df['Date'] = pd.to_datetime(df['Date'])
+   df1 = df.loc[df.Date>='2018-01-01', ['Date', 'Open', 'Close', 'Adjusted Close']]
+   df1 = df1.astype({'Open': 'float64', 'Close': 'float64', 'Adjusted Close': 'float64'})
 
-   p = figure(plot_width=800 ,plot_height=800, x_axis_type="datetime")
-   p.background_fill_color="#f5f5f5"
-   p.grid.grid_line_color="white"
-   p.title.text = 'Monthly Stock Data of %s' % app.vars['symbol'].upper()
-   p.xaxis.axis_label = "Date and Month of 2020"
-   p.yaxis.axis_label = "Price"
-   p.axis.axis_line_color = None
-   p.title.text_font = "Times"
+   # Plot the graph in Bokeh
+   curdoc().clear()
+   p = figure(x_axis_type="datetime", plot_width=1200, plot_height=600, title='Stock Price Tracking', toolbar_location="below")
+   p.title.text = 'Daily Stock Price Tracking of %s'  %app.vars['symbol'].upper()
+   p.xaxis.axis_label = "Date and Year"
+   p.yaxis.axis_label = "Stock Price"
    p.title.text_font_size = "20px"
-
+   
    if request.form.get('Open'):
-      p.line(df1['index'], df1['Open'], line_width=2, line_color='blue', legend_label='Open')
+      p.line(df1['Date'], df1['Open'], line_width=3, line_color='blue', alpha=0.5, legend_label='Open')
 
    if request.form.get('Close'):
-      p.line(df1['index'], df1['Close'], line_width=2, line_color='green', legend_label='Close')
+      p.line(df1['Date'], df1['Close'], line_width=3, line_color='green', alpha=0.5, legend_label='Close')
+   
+   if request.form.get('Adjusted Close'):
+      p.line(df1['Date'], df1['Adjusted Close'], line_width=3, line_color='orange', alpha=0.5, legend_label='Adjusted Close')
+    
 
-   # if request.form.get('Adj Close'):
-   #    p.line(df1['index'], df1['Adj Close'], line_width=2, line_color='red')
+   p.add_tools(HoverTool(
+     tooltips=[
+        ( 'Date',   '@x{%F}'     ),
+        ( 'Price',  '$@y{%0.2f}' )
+     ],
 
-   # p.add_tools(HoverTool(
-   #    tooltips=[
-   #       ( 'Date',   '@x{%F}'     ),
-   #       ( 'Open',  '$@y{%0.2f}' ),
-   #       ( 'Close',  '$@y{%0.2f}' ),
-   #       ( 'Adj Close',  '$@y{%0.2f}' ),
-   #    ],
+     formatters={
+        '@x'      : 'datetime',
+        '@y'     : 'printf',
+     },
 
-   #    formatters={
-   #       '@x'      : 'datetime',
-   #       '@y'     : 'printf',
-   #    },
-
-   #    mode='vline'
-   # ))
+     mode='mouse'
+   ))
 
    script, div = components(p)
    return render_template('graph.html', script=script, div=div)
 
+
+
 if __name__ == '__main__':
-   app.run(port=33507)
+  app.run(port=5000)
